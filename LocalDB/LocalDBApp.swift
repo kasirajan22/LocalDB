43b49e8b3f5efb5376095b4bc1d470d109912a1e
kasirajan22 <kasi.srm@gmail.com>
2020-10-21 20:51:02 +0530

    Initial Commit

---
+++ LocalDB/LocalDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalDBApp.swift
+//  LocalDB
+//
+//  Created by Magizhan on 21/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LocalDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
